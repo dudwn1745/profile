46d3cd377af4c8291fd66662f6c0a7a8b555e4c4
juju1227 <juju_1227@naver.com>
2023-09-05 23:50:42 +0900

    줄맞춤

--- Service/AuthService.cs
+++ Service/AuthService.cs
@@ -7,17 +7,17 @@ public class AuthService
     private readonly IHttpContextAccessor _context;
 
     public AuthService(IHttpContextAccessor context)
-	{
+    {
         _context = context;
-	}
+    }
 
-	/// <summary>
-	/// 로그인 설정
-	/// </summary>
-	/// <param name="values">claim 데이터</param>
-	/// <returns></returns>
+    /// <summary>
+    /// 로그인 설정
+    /// </summary>
+    /// <param name="values">claim 데이터</param>
+    /// <returns></returns>
     public async Task SetLogIn(IDictionary<string, string> values)
-	{
+    {
         if (values.Count == 0)
             return;
 
@@ -29,25 +29,25 @@ public class AuthService
 
         var claimsidentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
         await _context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsidentity));
-	}
+    }
 
-	/// <summary>
-	/// 로그아웃
-	/// </summary>
-	/// <returns></returns>
-	public async Task SetLogOut()
-	{
+    /// <summary>
+    /// 로그아웃
+    /// </summary>
+    /// <returns></returns>
+    public async Task SetLogOut()
+    {
         await _context.HttpContext.SignOutAsync();
         _context.HttpContext.Response.Redirect("/Login");
-	}
+    }
 
-	/// <summary>
-	/// 세팅된 claim 인증 관련 데이터 변경
-	/// </summary>
-	/// <param name="values">claim 데이터</param>
-	/// <returns></returns>
-	public async Task SetChangeClaims(IDictionary<string, string> values)
-	{//
+    /// <summary>
+    /// 세팅된 claim 인증 관련 데이터 변경
+    /// </summary>
+    /// <param name="values">claim 데이터</param>
+    /// <returns></returns>
+    public async Task SetChangeClaims(IDictionary<string, string> values)
+    {
         var identity = _context.HttpContext.User.Identity as ClaimsIdentity;
         if (identity.Claims == null || identity.Claims.Count() == 0) return;
 
@@ -64,5 +64,5 @@ public class AuthService
         }
 
         await _context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
-	}
+    }
 }
