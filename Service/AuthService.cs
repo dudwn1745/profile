6d1a59ca61ac451ab78331fb393cf95b54c09329
juju1227 <juju_1227@naver.com>
2023-09-05 23:45:28 +0900

    claim auth 관련 신규 클래스 추가

---
+++ Service/AuthService.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using auth.Models;
+
+public class AuthService
+{
+    private readonly IHttpContextAccessor _context;
+
+	public AuthService(IHttpContextAccessor context)
+	{
+		_context = context;
+	}
+
+	/// <summary>
+	/// 로그인 설정
+	/// </summary>
+	/// <param name="values">claim 데이터</param>
+	/// <returns></returns>
+	public async Task SetLogIn(IDictionary<string, string> values)
+	{
+		if (values.Count == 0)
+			return;
+
+		List<Claim> claims = new List<Claim>();
+		foreach (var claim in values)
+		{
+			claims.Add(new Claim(claim.Key, claim.Value));
+		}
+
+		var claimsidentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+		await _context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsidentity));
+	}
+
+	/// <summary>
+	/// 로그아웃
+	/// </summary>
+	/// <returns></returns>
+	public async Task SetLogOut()
+	{
+		await _context.HttpContext.SignOutAsync();
+		_context.HttpContext.Response.Redirect("/Login");
+	}
+
+	/// <summary>
+	/// 세팅된 claim 인증 관련 데이터 변경
+	/// </summary>
+	/// <param name="values">claim 데이터</param>
+	/// <returns></returns>
+	public async Task SetChangeClaims(IDictionary<string, string> values)
+	{
+		var identity = _context.HttpContext.User.Identity as ClaimsIdentity;
+		if (identity.Claims == null || identity.Claims.Count() == 0) return;
+
+		foreach (var claim in values)
+		{
+			var type = identity.FindFirst(claim.Key);
+			if (type != null)
+			{
+				if (identity.TryRemoveClaim(type))
+				{
+					identity.AddClaim(new Claim(claim.Key, claim.Value));
+				}
+			}
+		}
+
+		await _context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+	}
+}
