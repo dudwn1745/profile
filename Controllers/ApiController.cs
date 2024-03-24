be5869c97949cd47ca8e715d256ca69dc4422e20
juju1227 <juju_1227@naver.com>
2024-03-24 17:56:32 +0900

    restful api 중 get 방식 핸들러 작업 추가

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using auth.Models;
+using MediatR;
+
+namespace auth.Controllers;
+
+/// <summary>
+/// API 관련 컨트롤러
+/// </summary>
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class ApiController : ControllerBase
+{
+    private ISender _mediator;
+	protected ISender Mediator => _mediator ?? (_mediator = ServiceProviderServiceExtensions.GetRequiredService<ISender>(base.HttpContext.RequestServices));
+
+    /// <summary>
+    /// 액세스 토큰 생성
+    /// </summary>
+    /// <param name="requestModel"></param>
+    /// <returns></returns>
+    [HttpPost("authorize")]
+    public async Task<IActionResult> Authorize(AuthRequestModel requestModel)
+    {
+        var result = await Mediator.Send(new AuthorizeCommand()
+        {
+            UserId = requestModel.UserId,
+            Password = requestModel.Password
+        });
+
+        return Ok(new { result.AccessToken });
+    }
+
+    /// <summary>
+    /// 유저 정보를 조회
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetUserInfo()
+    {
+        return Ok(await Mediator.Send(new GetUserInfoQuery()));
+    }
+}
---
+++ Handler/GetUserInfoQuery.cs
@@ -0,0 +1,37 @@
+
+namespace Ksop.App.Application.Pub.Queries
+{
+    [Authorize]
+    public class GetUserInfoQuery : IRequest<UserInfoViewModel>
+    {
+        // 요청 파라미터를 정의합니다.
+        // Sample
+        // public string UserId { get; set; }
+    }
+
+    public class GetUserInfoQueryHandler : IRequestHandler<GetUserInfoQuery, UserInfoViewModel>
+    {
+        private readonly IDbContext _context;
+        private readonly IDateTime _date;
+
+        public GetUserInfoQueryHandler(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserInfoViewModel>> Handle(GetUserInfoQuery request, CancellationToken cancellationToken)
+        {
+            return await (
+                   from ui in _context.UserInfo
+                   join ua in _context.UserAuth on ui.Id equals ua.Id
+                   where ui.Enabled
+                   orderby pe.Id descending
+                   select new UserInfoViewModel
+                   {
+                       UserName = ui.UserName,
+                       UserId = ui.UserId,
+                       IsAdmin = ua.IsAdmin
+                   }).FirstOrDefaultAsync();
+        }
+    }
+}
